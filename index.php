94cbda974a62dded26751ac59ba67d5666aa595d
Mostepha Amine Bdeira <49349658+MABDesigns@users.noreply.github.com>
2022-09-28 00:57:37 +0100

    Updated

---
+++ index.php
@@ -0,0 +1,525 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<!-- Basic -->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>MAB Designs - Mostepha Amine Bdeira</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	
+	<!-- Load Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script%7CPoppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+	
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/basic.css" />
+	<link rel="stylesheet" href="css/layout.css" />
+	<link rel="stylesheet" href="css/blogs.css" />
+	<link rel="stylesheet" href="css/ionicons.css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" />
+	<link rel="stylesheet" href="css/animate.css" />
+	
+	<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="images/favicons/favicon.ico">
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125314689-11"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-125314689-11');
+	</script>
+	
+</head>
+
+<body>
+	
+	<!-- Page -->
+	<div class="page">
+
+		<!-- Preloader -->
+		<div class="preloader">
+			<div class="centrize full-width">
+				<div class="vertical-center">
+					<div class="spinner">
+						<div class="double-bounce1"></div>
+						<div class="double-bounce2"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<header class="header">
+			<div class="fw">
+				<div class="logo">
+					<a href="#">MAB Designs</a>
+				</div>
+				<a href="#" class="menu-btn"><span></span></a>
+				<div class="top-menu">
+					<ul>
+						<li><a href="#about-section">About</a></li>
+						<li><a href="#services-section">What I Do</a></li>
+						<li><a href="#works-section">Works</a></li>
+						<li><a href="#contact-section">Contact</a></li>
+					</ul>
+					<a href="#" class="close"></a>
+				</div>
+			</div>
+		</header>
+		
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Section Started -->
+			<div class="section started">
+				<div class="slide" style="background-image: url(images/slide-bg-2.jpg);"></div>
+				<div class="centrize full-width">
+					<div class="vertical-center">
+						<div class="st-title align-center">
+							<div class="typing-title">
+								<p>Developer</p>
+								<p>Designer</p>
+								<p>Coder</p>
+								<p>Freelancer</p>
+							</div>
+							<span class="typed-title"></span>
+						</div>
+						<div class="socials">
+							<a target="blank" href="https://www.facebook.com/likeab0ss1"><i class="icon ion ion-social-facebook"></i></a>
+							<a target="blank" href="https://github.com/MABDesigns"><i class="icon ion ion-social-github"></i></a>
+							<a target="blank" href="https://twitter.com/Mbdeira"><i class="icon ion ion-social-twitter"></i></a>
+							<a target="blank" href="https://www.instagram.com/m4b_007"><i class="icon ion ion-social-instagram"></i></a>
+							<a target="blank" href="https://linkedin.com/in/m4b007"><i class="icon ion ion-social-linkedin"></i></a>
+						</div>
+					</div>
+				</div>
+				<a href="#" class="mouse-btn"><i class="icon ion ion-chevron-down"></i></a>
+			</div>
+
+			<!-- Section About -->
+			<div class="section about align-left" id="about-section">
+				<div class="fw">
+					<div class="row">
+						<div class="col col-m-12 col-t-4 col-d-4">
+							<div class="profile">
+								<img src="images/profile.jpg" alt="" />
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-8 col-d-8">
+							<div class="text-box">
+								<h1>I'm Mostepha Amine BDEIRA, <br />Web Designer & Web Developer <br />from Algeria.</h1>
+								<p>
+								Passionate full-stack engineer with 5+ years of hands-on experience in developing scalable websites /
+applications using a wide range of front-end and back-end skills like HTML, CSS, PHP, JavaScript, etc. Developed lot of websites such as Chadli Bendjedid University website and Bikaimport company.
+Looking to further enhance framework skills as the future full stack developer.
+								</p>
+							</div>
+							<div class="bts">
+								<a href="#" class="btn extra">Download Resume</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Section Services -->
+			<div class="section services gray align-left" id="services-section">
+				<div class="fw">
+					<div class="titles">
+						<div class="title">What I Do</div>
+					</div>
+					<div class="row">
+						<div class="col col-m-12 col-t-6 col-d-4">
+							<div class="service-item">
+								<div class="circle"><i class="icon ion ion-ios-browsers-outline"></i></div>
+								<div class="name">UI/UX Design</div>
+								<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4">
+							<div class="service-item">
+								<div class="circle"><i class="icon ion ion-ios-lightbulb-outline"></i></div>
+								<div class="name">Brand Identity</div>
+								<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4">
+							<div class="service-item">
+								<div class="circle"><i class="icon ion ion-ios-color-wand-outline"></i></div>
+								<div class="name">Web Design</div>
+								<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4">
+							<div class="service-item">
+								<div class="circle"><i class="icon ion ion-social-android-outline"></i></div>
+								<div class="name">Mobile Apps</div>
+								<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4">
+							<div class="service-item">
+								<div class="circle"><i class="icon ion ion-ios-analytics-outline"></i></div>
+								<div class="name">Analytics</div>
+								<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4">
+							<div class="service-item">
+								<div class="circle"><i class="icon ion ion-ios-camera-outline"></i></div>
+								<div class="name">Apps</div>
+								<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Portfolio -->
+			<div class="section works align-left" id="works-section">
+				<div class="fw">
+					<div class="titles">
+						<div class="title">Recent Works</div>
+					</div>
+					<div class="filters">
+						<div class="f_btn active">
+							<label><input type="radio" name="fl_radio" value="box-item" />All</label>
+						</div>
+						<div class="f_btn">
+							<label><input type="radio" name="fl_radio" value="f-branding" />Branding</label>
+						</div>
+						<div class="f_btn">
+							<label><input type="radio" name="fl_radio" value="f-ui" />UI</label>
+						</div>
+						<div class="f_btn">
+							<label><input type="radio" name="fl_radio" value="f-photo" />Apps</label>
+						</div>
+					</div>
+					<div class="row box-items">
+						<div class="col col-m-12 col-t-6 col-d-4 box-item f-ui">
+							<div class="item">
+								<div class="desc">
+									<div class="image">
+										<a href="#popup-1" class="has-popup"><img src="images/works/work1.jpg" alt="" /></a>
+									</div>
+								</div>
+							</div>
+							<div id="popup-1" class="popup-box mfp-fade mfp-hide">
+								<div class="content">
+									<div class="image">
+										<img src="images/works/work1.jpg" alt="">
+									</div>
+									<div class="desc">
+										<div class="category">UI</div>
+										<h4>UI Design of Johnathan Specter Portfolio</h4>
+										<p>
+Demo UI Design of Johnathan Specter Portfolio 
+									</p>
+										<a href="https://github.com/MABDesigns/JohnathanSpecterPortfolio" class="btn" rel="follow" target="_blank">View Project</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4 box-item f-branding">
+							<div class="item">
+								<div class="desc">
+									<div class="image">
+										<a href="#popup-2" class="has-popup"><img src="images/works/work2.png" alt="" /></a>
+									</div>
+								</div>
+							</div>
+							<div id="popup-2" class="popup-box mfp-fade mfp-hide">
+								<div class="content">
+									<div class="image">
+										<img src="images/works/work2.png" alt="">
+									</div>
+									<div class="desc">
+										<div class="category">Branding</div>
+										<h4>Bika Import Company Logo Design</h4>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4 box-item f-ui">
+							<div class="item">
+								<div class="desc">
+									<div class="image">
+										<a href="#popup-3" class="has-popup"><img src="images/works/work3.jpg" alt="" /></a>
+									</div>
+								</div>
+							</div>
+							<div id="popup-3" class="popup-box mfp-fade mfp-hide">
+								<div class="content">
+									<div class="image">
+										<img src="images/works/work3.jpg" alt="">
+									</div>
+									<div class="desc">
+										<div class="category">UI</div>
+										<h4>YelpCamp UI Design</h4>
+										<p>
+Demo UI Design of YelpCamp Website 
+									</p>
+										<a href="https://github.com/MABDesigns/YeldCampWebsite" rel="follow" target="_blank" class="btn">View Project</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4 box-item f-photo">
+							<div class="item">
+								<div class="desc">
+									<div class="image">
+										<a href="#popup-4" class="has-popup"><img src="images/works/work4.png" alt="" /></a>
+									</div>
+								</div>
+							</div>
+							<div id="popup-4" class="popup-box mfp-fade mfp-hide">
+								<div class="content">
+									<div class="image">
+										<img src="images/works/work4.png" alt="">
+									</div>
+									<div class="desc">
+										<div class="category">Apps</div>
+										<h4>Chadli Bendjedid University Library Search</h4>
+										<p>
+										A simple web application for library search online of Chadli Bendjedid University.
+										</p>
+										<a href="http://univ-eltarf.dz/ucbet/bibliotheque/" rel="follow" target="_blank" class="btn">View Project</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4 box-item f-ui">
+							<div class="item">
+								<div class="desc">
+									<div class="image">
+										<a href="#popup-5" class="has-popup"><img src="images/works/work5.png" alt="" /></a>
+									</div>
+								</div>
+							</div>
+							<div id="popup-5" class="popup-box mfp-fade mfp-hide">
+								<div class="content">
+									<div class="image">
+										<img src="images/works/work5.png" alt="">
+									</div>
+									<div class="desc">
+										<div class="category">UI</div>
+										<h4>Chirp Website Demo</h4>
+										<p>
+										</p>
+										<a href="https://github.com/MABDesigns/ChirpWebsiteDemo" target="_blank" rel="follow" class="btn">View Project</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4 box-item f-ui f-photo">
+							<div class="item">
+								<div class="desc">
+									<div class="image">
+										<a href="#popup-6" class="has-popup"><img src="images/works/work6.png" alt="" /></a>
+									</div>
+								</div>
+							</div>
+							<div id="popup-6" class="popup-box mfp-fade mfp-hide">
+								<div class="content">
+									<div class="image">
+										<img src="images/works/work6.png" alt="">
+									</div>
+									<div class="desc">
+										<div class="category">UI+Apps</div>
+										<h4>Chadli Bendjedid University Official Website</h4>
+										<p>
+									The new design of Chadli Bendjedid University Website
+									</p>
+										<a href="http://univ-eltarf.dz" target="_blank" rel="follow" class="btn">View Project</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4 box-item f-ui f-photo">
+							<div class="item">
+								<div class="desc">
+									<div class="image">
+										<a href="#popup-7" class="has-popup"><img src="images/works/work7.png" alt="" /></a>
+									</div>
+								</div>
+							</div>
+							<div id="popup-7" class="popup-box mfp-fade mfp-hide">
+								<div class="content">
+									<div class="image">
+										<img src="images/works/work7.png" alt="">
+									</div>
+									<div class="desc">
+										<div class="category">UI+Apps</div>
+										<h4>BikaImport Company Official Website</h4>
+										<p>
+The official website of Bika Import Company.										</p>
+										<a href="https://bikaimport.com" target="_blank" rel="follow" class="btn">View Project</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4 box-item f-photo">
+							<div class="item">
+								<div class="desc">
+									<div class="image">
+										<a href="#popup-8" class="has-popup"><img src="images/works/work8.png" alt="" /></a>
+									</div>
+								</div>
+							</div>
+							<div id="popup-8" class="popup-box mfp-fade mfp-hide">
+								<div class="content">
+									<div class="image">
+										<img src="images/works/work8.png" alt="">
+									</div>
+									<div class="desc">
+										<div class="category">Apps</div>
+										<h4>OS Detector On Browser</h4>
+										<p>
+A simple app that detects OS from browser.										</p>
+										<a href="https://github.com/MABDesigns/detect-os" target="_blank" rel="follow" class="btn">View Project</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4 box-item f-ui">
+							<div class="item">
+								<div class="desc">
+									<div class="image">
+										<a href="#popup-9" class="has-popup"><img src="images/works/work9.png" alt="" /></a>
+									</div>
+								</div>
+							</div>
+							<div id="popup-9" class="popup-box mfp-fade mfp-hide">
+								<div class="content">
+									<div class="image">
+										<img src="images/works/work9.png" alt="">
+									</div>
+									<div class="desc">
+										<div class="category">UI</div>
+										<h4>UI design of simple Loginpanel</h4>
+										<p>
+Simple UI design for Loginpanel										</p>
+										<a href="https://github.com/MABDesigns/loginpanel-v1" target="_blank" rel="follow" class="btn">View Project</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col col-m-12 col-t-6 col-d-4 box-item f-ui">
+							<div class="item">
+								<div class="desc">
+									<div class="image">
+										<a href="#popup-10" class="has-popup"><img src="images/works/work10.png" alt="" /></a>
+									</div>
+								</div>
+							</div>
+							<div id="popup-10" class="popup-box mfp-fade mfp-hide">
+								<div class="content">
+									<div class="image">
+										<img src="images/works/work10.png" alt="">
+									</div>
+									<div class="desc">
+										<div class="category">UI</div>
+										<h4>OnDeck Website UI Design</h4>
+										<p>
+OnDeck UI Design 										</p>
+										<a href="https://github.com/MABDesigns/OndeckLandingPage" taget="_blank" rel="follow" class="btn">View Project</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="clear"></div>
+				</div>
+			</div>
+			
+			
+
+			<!-- Section Contacts -->
+			<div class="section contacts align-left" id="contact-section">
+				<div class="fw">
+					<div class="titles">
+						<div class="title">Contact Me</div>
+					</div>
+					<div class="contact-form">
+						<form id="cform" method="post">
+							<div class="row">
+								<div class="col col-m-12 col-t-6 col-d-6">
+									<div class="value">
+										<input type="text" name="name" placeholder="Name *" />
+									</div>
+								</div>
+								<div class="col col-m-12 col-t-6 col-d-6">
+									<div class="value">
+										<input type="tel" name="tel" placeholder="Phone *" />
+									</div>
+								</div>
+								<div class="col col-m-12 col-t-6 col-d-6">
+									<div class="value">
+										<input type="text" name="email" placeholder="Email *" />
+									</div>
+								</div>
+								<div class="col col-m-12 col-t-6 col-d-6">
+									<div class="value">
+										<input type="text" name="subject" placeholder="Subject *" />
+									</div>
+								</div>
+								<div class="col col-m-12 col-t-12 col-d-12">
+									<div class="value">
+										<textarea name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="col col-m-12 col-t-12 col-d-12">
+									<a href="#" class="btn" onclick="$('#cform').submit(); return false;">Send Message</a>
+								</div>
+							</div>
+						</form>
+						<div class="alert-success">
+							<p>Thanks, your message is sent successfully. We will contact you shortly!</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Footer -->
+			<footer class="align-center">
+				<div class="socials">
+					<a target="blank" href="https://www.facebook.com/likeab0ss1"><i class="icon ion ion-social-facebook"></i></a>
+					<a target="blank" href="https://github.com/MABDesigns"><i class="icon ion ion-social-github"></i></a>
+					<a target="blank" href="https://twitter.com/Mbdeira"><i class="icon ion ion-social-twitter"></i></a>
+					<a target="blank" href="https://www.instagram.com/m4b_007"><i class="icon ion ion-social-instagram"></i></a>
+					<a target="blank" href="https://linkedin.com/in/m4b007"><i class="icon ion ion-social-linkedin"></i></a>
+				</div>
+				<div class="copy">© 2022 Mostepha Amine Bdeira. all rights reserved.</div>
+			</footer>
+			
+		</div>
+
+	</div>
+	
+	<!-- jQuery Scripts -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.validate.js"></script>
+	<script src="js/masonry.pkgd.js"></script>
+	<script src="js/imagesloaded.pkgd.js"></script>
+	<script src="js/masonry-filter.js"></script>
+	<script src="js/magnific-popup.js"></script>
+	<script src="js/jquery.mb.YTPlayer.js"></script>
+	<script src="js/particles.js"></script>
+	<script src="js/typed.js"></script>
+	
+	<!-- Main Scripts -->
+	<script src="js/main.js"></script>
+	
+</body>
+</html>
